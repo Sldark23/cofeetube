12256afe672e9c05643e64979d7d5851e0ac20ca
Sldark23 <115877460+Sldark23@users.noreply.github.com>
2024-05-06 22:21:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
-// Função para carregar vídeos do arquivo JSON
-function carregarVideos() {
-  fetch('videos.json')
-    .then(response => response.json())
-    .then(data => exibirVideosAleatorios(data.videos))
-    .catch(error => console.error('Erro ao carregar vídeos:', error));
-}
+// Lista de vídeos
+const videos = [
+  {
+    title: "FRUTAS DO BLOX FRUITS VS FRUTAS DO ANIME ONE PIECE",
+    src: "https://www.youtube.com/embed/xZTFARpT2go"
+  },
+  {
+    title: "E se o Apocalipse Acontecer Hoje? / Explosão nuclear",
+    src: "https://www.youtube.com/embed/JbBGWcOqRao"
+  },
+  {
+    title: "Unleash The Live #3 - Steven Universo: Libere o Prisma",
+    src: "https://www.youtube.com/embed/656nhLzbrHQ"
+  },
+  {
+    title: "AJUDOU a vizinha DIVORCIADA e ela se APAIXONOU por ele!!",
+    src: "https://www.youtube.com/embed/JJsfPya13pk"
+  },
+  {
+    title: "Passei 24 Horas Debaixo D'água - Desafio",
+    src: "https://www.youtube.com/embed/6_im_hGxGRk"
+  },
+  {
+    title: "glamrocks react totheir all aftonstheory • tw in desc • og ✋😩",
+    src: "https://www.youtube.com/embed/ClKp5zIj3yc"
+  },
+  {
+    title: "Tower of Fantasy - DON'T MAKE THESE MISTAKES!!!",
+    src: "https://www.youtube.com/embed/zejEqoB23Xg"
+  },
+  {
+    title: "Travis Scott - goosebumps ft. Kendrick Lamar",
+    src: "https://www.youtube.com/embed/xtsn5OLWV1k"
+  },
+  {
+    title: "Marshmello - Alone (Official Music Video)",
+    src: "https://www.youtube.com/embed/0uISYKI3ymk"
+  },
+  {
+    title: "Calvin Harris - This Is What You Came For (Official Video) ft. Rihanna",
+    src: "https://www.youtube.com/embed/nHh_PCUOQw4"
+  },
+  {
+    title: "The Chainsmokers & Coldplay - Something Just Like This (Lyric)",
+    src: "https://www.youtube.com/embed/GfdTfeWkptk"
+  },
+  {
+    title: "Daft Punk - Get Lucky (Official Audio) ft. Pharrell Williams, Nile Rodgers",
+    src: "https://www.youtube.com/embed/UV3R7eKqNFI"
+  },
+  {
+    title: "Ed Sheeran - Thinking Out Loud [Official Video]",
+    src: "https://www.youtube.com/embed/56h0lNdO-Kk"
+  },
+  {
+    title: "Camila Cabello - Havana ft. Young Thug [Official Video]",
+    src: "https://www.youtube.com/embed/kX3nB4PpJko"
+  }
+];
 
 // Função para exibir vídeos aleatórios na tela
 function exibirVideosAleatorios(videosList) {
@@ -55,5 +107,5 @@ function pesquisarVideos() {
   exibirVideosAleatorios(filteredVideos);
 }
 
-// Chamada inicial para carregar os vídeos ao carregar a página
-carregarVideos();
+// Chamada inicial para exibir vídeos aleatórios ao carregar a página
+exibirVideosAleatorios(videos);
